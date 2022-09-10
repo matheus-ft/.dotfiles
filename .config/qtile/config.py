1b29520274dfa46d5fb7768c18336045339c2d9f
Matheus Feres Turcheti <71662383+matheus-ft@users.noreply.github.com>
2022-09-09 21:30:09 -0300

    Update qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -4,7 +4,6 @@ import subprocess
 from typing import List
 from libqtile import qtile
 from libqtile.config import Click, Drag, Group, KeyChord, Key, Match, Screen
-from libqtile.command import lazy
 from libqtile import layout, bar, hook
 from libqtile.lazy import lazy
 from libqtile.dgroups import simple_key_binder
@@ -108,18 +107,8 @@ keys = [
         lazy.spawn("toggle_keyboard_layout"),
         desc="Next keyboard layout.",
     ),
-    Key(
-        [],
-        "XF86MonBrightnessUp",
-        lazy.spawn("brightnessctl s +5%"),
-        desc="Increase brightness",
-    ),
-    Key(
-        [],
-        "XF86MonBrightnessDown",
-        lazy.spawn("brightnessctl s 5%-"),
-        desc="Decrease brightness",
-    ),
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl s +5%")),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl s 5%-")),
     Key([], "XF86AudioMute", lazy.spawn("amixer -q sset Master toggle")),
     Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -q sset Master 5%+")),
     Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -q sset Master 5%-")),
@@ -143,6 +132,16 @@ keys = [
     Key([modkey, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([modkey, "shift"], "r", lazy.restart(), desc="Restart Qtile"),
     Key([modkey, "shift"], "q", lazy.spawn(power_menu), desc="Show power menu"),
+    Key(
+        [modkey],
+        "F1",
+        lazy.spawn(
+            "sh -c 'echo \""
+            + show_keys(keys)  # noqa
+            + '" | rofi -dmenu -i -mesg "Keyboard shortcuts"\''
+        ),
+        desc="Print keyboard bindings",
+    ),
 ]
 
 # Allow MODKEY+[1 through 8] to bind to groups, see https://docs.qtile.org/en/stable/manual/config/groups.html
@@ -283,7 +282,7 @@ def widgets():
         ),
         widget.ThermalSensor(
             foreground_alert=colors["magenta"],
-            threshold=80,
+            threshold=75,
             fmt="🌡 {}",
             tag_sensor="Core 0",
             mouse_callbacks={"Button1": lambda: qtile.cmd_spawn(terminal + " -e htop")},
@@ -309,11 +308,11 @@ def widgets():
             mouse_callbacks={
                 "Button1": lambda: qtile.cmd_spawn(
                     browser + " https://calendar.google.com/calendar/u/0/r"
-                )
+                ),
             },
-            foreground=colors["black"],
+            foreground=colors["white"],
             decorations=[
-                RectDecoration(colour=colors["light_grey"], radius=10, filled=True)
+                RectDecoration(colour=colors["dark_grey"], radius=10, filled=True)
             ],
         ),
         widget.Spacer(lenght=bar.STRETCH),
@@ -389,7 +388,7 @@ def panel():
 
 screens = [main_panel(), panel()]
 
-dgroups_app_rules: List = []
+dgroups_app_rules: List = []  # this type hint is mandatory!
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
@@ -430,3 +429,61 @@ def start_once():
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
 wmname = "LG3D"
+
+
+def show_keys(keys):
+    """print current keybindings in a pretty way for a rofi/dmenu window.
+
+    Courtesy of LaurentOngaro
+    (https://github.com/qtile/qtile/issues/1329)
+    """
+    key_help = ""
+    keys_ignored = (
+        "XF86AudioMute",
+        "XF86AudioLowerVolume",
+        "XF86AudioRaiseVolume",
+        "XF86AudioPlay",
+        "XF86AudioNext",
+        "XF86AudioPrev",
+        "XF86AudioStop",
+        "XF86MonBrightnessDown",
+        "XF86MonBrightnessUp",
+    )
+    text_replaced = {
+        "mod4": "[Super]",
+        "control": "[Ctrl]",
+        "mod1": "[Alt]",
+        "shift": "[Shift]",
+        "twosuperior": "²",
+        "less": "<",
+        "ampersand": "&",
+        "Escape": "Esc",
+        "Return": "Enter",
+    }
+    for k in keys:
+        if k.key in keys_ignored:
+            continue
+
+        mods = ""
+        key = ""
+        desc = k.desc.title()
+        for m in k.modifiers:
+            if m in text_replaced.keys():
+                mods += text_replaced[m] + " + "
+            else:
+                mods += m.capitalize() + " + "
+
+        if len(k.key) > 1:
+            if k.key in text_replaced.keys():
+                key = text_replaced[k.key]
+            else:
+                key = k.key.title()
+        else:
+            key = k.key
+
+        key_line = "{:<30} {}".format(mods + key, desc + "\n")
+        key_help += key_line
+
+        # debug_print(key_line)  # debug only
+
+    return key_help
